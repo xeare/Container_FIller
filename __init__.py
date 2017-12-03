1eab27e7409257024a382ba519acc755ebfd9695
Henock T <33410004+xeare@users.noreply.github.com>
2017-12-02 19:40:29 -0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+class ContainerFiller:
+    conversion_dict = {
+                        teaspoons: {"pints": 100, "tablespoons": 2}
+                        }
+
+    @staticmethod
+    def calculate(teaspoons):
+        teaspoons = [100]
+        return teaspoons
+        # while teaspoons > 0:
+        #            if teaspoons % 'pints' == 0:
+        #                return pints
+        # pass
