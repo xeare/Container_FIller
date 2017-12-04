a901f48b3f415fe80fcd707dc64575f3b3878c4e
Henock T <33410004+xeare@users.noreply.github.com>
2017-12-03 19:49:00 -0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,13 +1,38 @@
 class ContainerFiller:
-    conversion_dict = {
-                        teaspoons: {"pints": 100, "tablespoons": 2}
-                        }
 
     @staticmethod
     def calculate(teaspoons):
-        teaspoons = [100]
-        return teaspoons
-        # while teaspoons > 0:
-        #            if teaspoons % 'pints' == 0:
-        #                return pints
-        # pass
+        teaspoons = 100
+        pints = 96
+        tablespoons = 3
+        remaining_water = teaspoons
+        while remaining_water != 0:
+            remaining_water = teaspoons % pints  # remaining_water = 4
+            if remaining_water < 96:
+                number_of_pints = 1
+                remaining_water = remaining_water % tablespoons
+                # the above is remaining_water = 4 % 3 --> 1
+                if remaining_water == 1:
+                    number_of_tablespoons = 1
+                    remaining_water = remaining_water % 1
+                    # remaining_water = 1 % 1 -->0
+                    number_of_teaspoons = 1
+                    if remaining_water == 0:
+                        return (('pints', number_of_pints),
+                                ('tablespoons', number_of_tablespoons),
+                                ('teaspoons', number_of_teaspoons))
+                    else:
+                        return None
+                else:
+                    return None
+            else:
+                return None
+
+        def calculate(teaspoons):
+            teaspoons = 1
+            remaining_water = ()
+            while remaining_water != 0:
+                remaining_water = teaspoons % teaspoons
+                if remaining_water == 0:
+                    number_of_teaspoons = 1
+                    return (('teaspoons', number_of_teaspoons))
