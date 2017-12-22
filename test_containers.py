bb70c27bc3356bae8b59eea9bb4fbcd9be33cb03
Henock T <33410004+xeare@users.noreply.github.com>
2017-12-22 16:28:03 -0700

    Add files via upload

--- test_containers.py
+++ test_containers.py
@@ -1,14 +1,14 @@
-from unittest import TestCase, main, skip
+from unittest import TestCase, main
 
-from expects import expect, equal
+from expects import expect, equal, raise_error
 
-from container_filler import ContainerFiller
+from container_filler import ContainerFiller, WrongInputType
 
 
 class TestContainers(TestCase):
 
     # @skip('')
-    def test_leaves_no_air(self):
+    def test_one_hundred_teaspoons(self):
         # sut = system under test, in this case the containers
         sut = ContainerFiller()
         containers = sut.calculate(teaspoons=100)
@@ -19,20 +19,27 @@ class TestContainers(TestCase):
                     ('teaspoons', 1)])))
 
     # @skip('')
-    def test_string_as_input(self):
+    def test_raises_wrong_input_type_on_str(self):
         sut = ContainerFiller()
-        try:
+
+        def attempt():
             sut.calculate(teaspoons=('aardvark'))
-            assert False, 'No exception was raised'
-        except TypeError:
-            pass
+        expect(attempt).to(raise_error(WrongInputType))
 
     # @skip('')
-    def test_exception_error_text(self):
-        try:
-            pass
-        except WrongInputType as e:
-            expect(str(e)).to(equal('Please give a number'))
+    def test_exception_msg_on_str(self):
+        sut = ContainerFiller()
+
+        def attempt():
+            sut.calculate(teaspoons=('aardvark'))
+        expect(attempt).to(raise_error(Exception, 'Please give a number'))
+
+    def test_raises_wrong_input_type_on_dict(self):
+        sut = ContainerFiller()
+
+        def attempt():
+            sut.calculate(teaspoons=({'spam': 42, 'eggs': 'ximinez'}))
+        expect(attempt).to(raise_error(WrongInputType))
 
     # @skip('')
     def test_ten_thousand_teaspoons(self):
@@ -40,8 +47,9 @@ class TestContainers(TestCase):
         containers = sut.calculate(teaspoons=10000)
         expect(containers).to(equal(
             (
-                [('pints', 104),
-                    ('tablespoons', 5),
+                [('gallons', 13),
+                    ('fluid ounces', 2),
+                    ('tablespoons', 1),
                     ('teaspoons', 1)])))
 
     # @skip('')
@@ -53,9 +61,17 @@ class TestContainers(TestCase):
                 [('teaspoons', 1)])
         ))
 
-    @skip('')
-    def test_contains_all_water(self):
-        pass
+    def test_twenty_five_thousand_teaspoons(self):
+        sut = ContainerFiller()
+        containers = sut.calculate(teaspoons=25000)
+        expect(containers).to(equal(
+            (
+                [('barrels', 1),
+                    ('gallons', 1),
+                    ('gills', 1),
+                    ('fluid ounces', 2),
+                    ('tablespoons', 1),
+                    ('teaspoons', 1)])))
 
 
 if "__main__" == __name__:
