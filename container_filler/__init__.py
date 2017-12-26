64575e49cb85164587fd7f8233423390871a20c3
Henock <teshome0@gmail.com>
2017-12-26 10:03:46 -0700

    refactored all tests run fine

--- container_filler/__init__.py
+++ container_filler/__init__.py
@@ -15,29 +15,14 @@ class WrongInputType(Exception):
 
 class ContainerFiller:
     def calculate(self, teaspoons):
-
+        end_result = []
         if not isinstance(teaspoons, int):
             raise WrongInputType("Please give a number")
 
         for value, unit in conversion:
-            #  conversion.sort(key=itemgetter(0), reverse=True)
             filled = int(teaspoons/value)
             teaspoons_remaining = teaspoons % value
             teaspoons = teaspoons_remaining
             if filled:
-                return [(unit, filled)]
-
-
-'''
-possible?
-    def _div_and_mod(self,teaspoons, conversion):
-        filled = int(teaspoons / conversion)
-        teaspoons_remaining = teaspoons % conversion
-        return filled, teaspoons_remaining
-    def calculate(self, teaspoons):
-        for value, unit in conversion:
-        unit, teaspoons_remaining = self._div_and_mod(teaspoons, conversion)
-            if filled:
-                teaspoons = teaspoons_remaining
-                print ((unit, filled))
-'''
+                end_result = end_result + [(unit,filled)]
+        return end_result
