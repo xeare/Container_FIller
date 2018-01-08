31700191d2ce1be82ea0db8c4f2373af0452ba20
Henock <teshome0@gmail.com>
2018-01-08 02:46:13 -0700

    added a jinja page that currently does not work. but is rightly connected to the flask app. I also have a working selenium webdriver chrome code.

--- service.py
+++ service.py
@@ -5,15 +5,18 @@ from container_filler import ContainerFiller
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def form():
     return render_template('index.html')
 
+
 @app.route('/submit', methods=['POST'])
 def submit():
-    return str(ContainerFiller().calculate(int(request.form['teaspoons'])))
+    result = str(
+        ContainerFiller().calculate(int(request.form['teaspoons'])))
+    return render_template('gallon.html', value=result)
 
-@app.route('/submit')
-def submit_display():
-    if end_result == [('barrels', 1)]:
-        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
